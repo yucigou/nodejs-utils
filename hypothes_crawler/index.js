4ef62173f3de5a5aa32e8ee79137f8f2f941b32c
Yuci Gou <yuci.gou@gmail.com>
2018-09-07 22:12:44 +0100

    Retrieve all PMIDs

--- hypothes_crawler/index.js
+++ hypothes_crawler/index.js
@@ -4,7 +4,7 @@ const fs = require('fs')
 const baseUrl = 'https://hypothes.is/groups/NMb8iAjd/europe-pmc'
 const numberOfPages = 39
 const pmidFileName = 'pmids.txt'
-const re = /europepmc.org\/abstract\/MED\/(\d+)/g;
+const re = /(europepmc.org\/abstract\/MED\/\d+)/g;
 const urls = []
 
 fs.truncateSync(pmidFileName, 0)
@@ -21,37 +21,26 @@ console.log(`Number of urls: ${urls.length}`)
 
 const writeToFile = pmidSet => {
 	let pmidList = Array.from(pmidSet).reduce((a, b) => a + b + '\n', '');
-	console.log('Write to file...: ', pmidList.length)
+	console.log('Write to file...: ', pmidSet.size)
 	fs.writeFileSync(pmidFileName, pmidList);
 }
 
-const crawl = urls => {
+const crawl_ = urls => {
 	var obj = new Promise((resolve, reject) => {
 		const pmidSet = new Set()
-		// (async ()=>{		
-		// 	for (let i = 0; i < urls.length; i++) {
-		// 		let url = urls[i];
-		// 		console.log('Handle URL: ', url)
-		// 		let response = await axios({url})
-
-		// 		let m
-		// 		do {
-		// 		    m = re.exec(response.text);
-		// 		    if (m) {
-		// 		        pmidSet.add(m[1])
-		// 		    }
-		// 		} while (m);
-		// 		console.log("Done with URL: " + url)
-		// 		console.log("Go deal with next URL")	
-		// 	}
-		// })();
-		// console.log("To be resolved")
-		// resolve(pmidSet)
-
 		let count = 0;
-		urls.forEach(async url => {
-			console.log(url)
-			let response = await axios({url})
+		urls.forEach(async (url, index) => {
+			console.log(`Requesting URL: ${url}`)
+			let response;
+			try {
+				response = await axios({url})
+			} catch (error) {
+				count++
+				console.log(`Error with URL: ${url} (${count}, page ${index}), ${error}`)
+				return;
+			}
+			count++
+			console.log(`Done with URL: ${url} (${count}, page ${index})`)
 
 			let m
 			do {
@@ -61,12 +50,40 @@ const crawl = urls => {
 			    }
 			} while (m);
 
-			count++;
-			console.log(`Done with URL: ${url} (${count})`)
 			if (count >= urls.length) {
+				console.log(`${count} pages done. Resolving promise.`)
 				resolve(pmidSet)				
 			}
 		})
+	})
+	return obj
+}
+
+const crawl = urls => {
+	var obj = new Promise((resolve, reject) => {
+		(async ()=>{		
+			const pmidSet = new Set()
+			for (let i = 0; i < urls.length; i++) {
+				let url = urls[i];
+				console.log('Handle URL: ', url)
+				let response = await axios({url})
+				
+				let m
+				do {
+				    m = re.exec(response.data);
+				    if (m) {
+				        pmidSet.add(m[1])
+				    }
+				} while (m);
+				
+				if (i === urls.length - 1) {
+					console.log("To be resolved")
+					resolve(pmidSet)			
+				} else {
+					console.log("Go deal with next URL")
+				}
+			}
+		})();
 	})
 	return obj
 }
