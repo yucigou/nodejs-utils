572293144108a3868dae57ccf26887aaa4398b21
Yuci Gou <yuci.gou@gmail.com>
2018-09-09 00:25:53 +0100

    Generate doi to links mappings

--- hypothes_crawler/mapdoilinks.js
+++ hypothes_crawler/mapdoilinks.js
@@ -1,5 +1,16 @@
 const axios = require('axios')
+const fs = require('fs')
+const readlines = require('n-readlines');
+const writer = require('./modules/writer')
 
+// const inputFilename = './testdata.txt'
+const inputFilename = './pmids.txt'
+const outputFilename = './mappings.txt'
+const errorFilename = './errors.txt'
+
+writer.clearFile(errorFilename)
+
+const liner = new readlines(inputFilename);
 const domain = 'europepmc.org'
 const sub_domains = ['', 'beta.', 'test.', 'dev.', 'staging.']
 const protocols = ['http', 'https']
@@ -23,7 +34,6 @@ const createUrlList = (pmid, pmcid) => {
         }).reduce((acc, val) => acc.concat(val), [])
         urls.push(...pmcidUrls)
     }
-    // console.log(urls)
     return urls
 }
 
@@ -33,18 +43,44 @@ const createMapping = (doi, urls) => {
 
 const crawl = async (src, ext_id) => {
     const url = `https://www.ebi.ac.uk/europepmc/webservices/rest/search?query=src%3A${src}%20and%20ext_id%3A${ext_id}&resultType=lite&synonym=FALSE&cursorMark=*&pageSize=25&format=json`
+    console.log(`Handling URL: search?query=src%3A${src}%20and%20ext_id%3A${ext_id}&resultType=lite&synonym=FALSE&cursorMark=*&pageSize=25&format=json`)    
     const response = await axios({url})
-    // const mapping = {doi, }
+    if (response.data.resultList.result.length === 0) {
+        return null;
+    }
     const doi = response.data.resultList.result[0].doi
     const pmid = ext_id
     const pmcid = response.data.resultList.result[0].pmcid
-    // console.log(response.data.resultList.result[0])
     return {doi, pmid, pmcid}
 }
 
-(async()=>{
-    let ids = await crawl('MED', '24806729')
-    let urls = createUrlList(ids.pmid, ids.pmcid)
-    let mapping = createMapping(ids.doi, urls)
-    console.log(mapping)
+const re = /europepmc.org\/abstract\/MED\/(\d+)/ig;
+const mappings = []
+
+const processAllPmid = async () => {
+    let count = 0
+    let line
+    while (line = liner.next()) {
+        let m
+        do {
+            m = re.exec(line);
+            if (m) {
+                let ids = await crawl('MED', m[1])
+                if (ids) {
+                    let urls = createUrlList(ids.pmid, ids.pmcid)
+                    let mapping = createMapping(ids.doi, urls)
+                    mappings.push(mapping)    
+                } else {
+                    fs.appendFileSync(errorFilename, line + '\n');
+                }
+            }
+        } while (m);
+        console.log(`Done (${++count})`)
+    }
+}
+
+(async () => {
+    await processAllPmid()
+    console.log(`--------------------DONE (${mappings.length})------------------------`)
+    fs.writeFileSync(outputFilename, JSON.stringify(mappings, null, 2));
 })();
